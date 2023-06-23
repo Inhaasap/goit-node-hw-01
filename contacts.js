252b04a9a54e38dd1e1f0e37ec09115243fa4d5c
Інга Юрченко <missingusya@hotmail.com>
2023-06-23 14:56:06 +0300

    Update contacts.js
    Додала блок try...catch

--- contacts.js
+++ contacts.js
@@ -11,36 +11,49 @@ const listContacts = async () => {
 };
 
 const getContactById = async contactId => {
-  const contacts = await listContacts();
-  const result = contacts.find(item => item.id === contactId);
-  return result || null;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.error('Сталася помилка при отриманні контакту за його ID:', error.message);
+    return null;
+  }
 };
 
 const removeContact = async contactId => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(item => item.id === contactId);
-  if (index === -1) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.error('Сталася помилка при видаленні контакту:', error.message);
     return null;
   }
-  const [result] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
 };
 
 const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error('Сталася помилка при додаванні контакту:', error.message);
+    return null;
+  }
 };
-
-// crypto.randomUUID()
 
 module.exports = {
   listContacts,
